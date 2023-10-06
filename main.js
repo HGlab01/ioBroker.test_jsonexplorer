4c1c4b83b4bb0cb1d89e092916c5d797a0c4fe2c
HGlab <54232526+HGlab01@users.noreply.github.com>
2023-10-06 15:26:14 +0200

    remove is-online

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const isOnline = require('is-online');
 
 // Load your modules here, e.g.:
 const jsonExplorer = require('iobroker-jsonexplorer');
